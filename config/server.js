55a0b216e7b27766f76ffcaa3c3b7fd7023c918f
Praveen <prapok.se@gmail.com>
2020-08-18 14:01:31 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b868304d6be098fb70cf2afde4bef591'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bd99ad48-90a8-4fa3-9a64-97a22e482a3c'
+};
